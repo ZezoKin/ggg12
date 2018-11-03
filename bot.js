9df73737dae3fee831761cd5280c90b6f99728f4
ZezoKin <44434287+ZezoKin@users.noreply.github.com>
2018-11-03 18:38:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` أحــمـد الـوحــيــد  `,"http://twitch.tv/S-F")
+client.user.setGame(` ُأحَــمَـدُ الـوِحّــيِــد  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
